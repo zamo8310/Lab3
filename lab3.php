792b7b5ede5f0f1f3ecbf52d4aabe0e73fea199c
Jessen Winardinata <jewinardinata@csumb.edu>
2017-09-15 20:05:26 +0000

    adding Lab3

---
+++ lab3.php
@@ -0,0 +1,29 @@
+<?php
+$start = microtime(true);
+$deck = range(0,51);  //creates array with values 1 to 52
+
+$suits = array("clubs","spades","hearts","diamonds");
+
+shuffle($deck);
+
+foreach ($deck as $card) {
+    
+    echo "Card value: "  . (($card % 13) + 1) . "-  Card Suite: " .  $suits[floor($card / 13)] . " <br />";
+    
+}
+
+
+$elapsedSecs = microtime(true) - $start;
+echo $elapsedSecs . " secs";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+
+    </body>
+</html>
