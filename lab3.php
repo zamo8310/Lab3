87276d6118e2ec35d2c72d1b61a0f94e25bcafd8
theuzbekistan <dbeach@csumb.edu>
2017-09-15 20:40:26 +0000

    adding notes

--- lab3.php
+++ lab3.php
@@ -2,13 +2,10 @@
 $start = microtime(true);
 $deck = range(0,51);  //creates array with values 1 to 52
 
-
-
-
-
+//get random number 0 to 51
+//get card until it goes past 35
 
 $suits = array("clubs","spades","hearts","diamonds");
-
 
 //(($card % 13) + 1)
 //$suits[floor($card / 13)] 
